46ab1096949680cc7a3a5eabd6121dbd551ad2a2
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-08-30 20:25:11 +0530

    Refactor PatientMapper to handle DTO-to-entity and entity-to-DTO conversions consistently

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,20 +1,33 @@
 package com.pm.patientservice.mapper;
 
+import java.time.LocalDate;
+
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 
-public class PatientMapper { 
-	
+public class PatientMapper {
+
 	public static PatientResponseDTO toDTO(Patient patient) {
 		PatientResponseDTO patientDto = new PatientResponseDTO();
-		
+
 		patientDto.setId(patient.getId().toString());
 		patientDto.setName(patient.getName());
 		patientDto.setAddress(patient.getAddress());
 		patientDto.setEmail(patient.getEmail());
 		patientDto.setDateOfBirth(patient.getDateOfBirth().toString());
-		
+
 		return patientDto;
+	}
+
+	public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+		Patient patient = new Patient();
+		patient.setName(patientRequestDTO.getName());
+		patient.setAddress(patientRequestDTO.getAddress());
+		patient.setEmail(patientRequestDTO.getEmail());
+		patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+		patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+		return patient;
 	}
 
 }
