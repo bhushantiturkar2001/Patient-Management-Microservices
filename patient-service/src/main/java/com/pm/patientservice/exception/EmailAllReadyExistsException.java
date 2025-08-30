f8107ee08ab6f6d7518ce73663fd3cb055990e95
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-08-30 20:32:15 +0530

    Add custom exception for handling duplicate patient email scenario

---
+++ patient-service/src/main/java/com/pm/patientservice/exception/EmailAllReadyExistsException.java
@@ -0,0 +1,9 @@
+package com.pm.patientservice.exception;
+
+public class EmailAllReadyExistsException extends RuntimeException {
+
+	public EmailAllReadyExistsException(String message) {
+		super(message);
+	}
+
+}
