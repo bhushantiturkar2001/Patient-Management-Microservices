9caf313b864fc613da78b9b4877feba0e045d8c2
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2025-09-20 13:20:51 +0530

    Add Analytics Service with Kafka consumer, proto definitions, and base application setup

---
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,34 @@
+package com.pm.analyticsservice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer {
+
+	private final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+	@KafkaListener(topics = "patient", groupId = "analytics-service")
+	public void consumeEvent(byte[] event) {
+
+		try {
+			PatientEvent patientEvent = PatientEvent.parseFrom(event);
+			// ... perform any business related to analytics here
+
+			log.info("Recived patient event: [PatientId={},PatientName={},PatientEmail={}]",
+					patientEvent.getPatientId(), patientEvent.getName(), patientEvent.getEmail());
+			
+			
+		} catch (InvalidProtocolBufferException e) {
+			log.error("Error deserializing event {}", e.getMessage());
+		}
+
+	}
+
+}
